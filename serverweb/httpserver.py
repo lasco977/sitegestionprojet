bafdd69a6e6c26f1ad6b0949b66b96070ba39c8b
lasco977 <76695351+lasco977@users.noreply.github.com>
2021-04-30 18:33:34 +0200

    Add files via upload

---
+++ serverweb/httpserver.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import http.server            # importe un ensemble d'instructions pour servir les requêtes http.
+import socketserver         # importe un ensemble d'instructions pour connecter le programme.
+
+# Serveur http de base delivrant le contenu du repertoire courant via le port indique.
+PORT = 5432
+
+Handler = http.server.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("",PORT), Handler)
+print("à l'écoute sur le port :", PORT)
+httpd.serve_forever()
